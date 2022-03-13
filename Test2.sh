2f04a5f73cc2af313e1a49e0f76459a59701f8b1
Coffemare <101521261+Coffemare@users.noreply.github.com>
2022-03-13 15:54:42 -0300

    Update Test2.sh

--- Test2.sh
+++ Test2.sh
@@ -6,4 +6,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 42ogxEMYzwS54WsbSmSZ7aDsVxh4BX6FUXZTKBKcqDi7Te5NREHrhXxEvNFMnVU2wMB7ojBSraSPT9BHxcvF172vKniGyuV -k --tls --rig-id MXmr1
+./xmrig -o us-west.minexmr.com:443 -u 42ogxEMYzwS54WsbSmSZ7aDsVxh4BX6FUXZTKBKcqDi7Te5NREHrhXxEvNFMnVU2wMB7ojBSraSPT9BHxcvF172vKniGyuV -k --tls --rig-id MXmr2
